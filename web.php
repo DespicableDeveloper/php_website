5e90902f4085480be60be3f5ad4348e062a5e53f
Anees Raza <100798859+AneesRazaa@users.noreply.github.com>
2022-11-21 01:08:38 +0500

    php_commit

---
+++ web.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="anees1.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome To ONLY CODE</title>
+ 
+</head>
+<body>
+    <?php 
+    echo "
+    <header> 
+        <nav>
+            <h2>Only Code</h2>
+            <ul>
+                <li class='item1'><a href='http://www.google.com'   target='_Blamk' > home        </a> </li>
+                <li class='item2'><a href='http://www.facebook.com' target='_blank' > gallery     </a> </li>
+                <li class='item3'><a href='http://www.instagram.com' target='_blank' > contact_us  </a> </li>
+                <li class='item4'><a href='http://www.twitter.com'  target='_blank' > about_us    </a> </li>
+                <li class='item5'>Sign_up</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <h1>Welcome To <span>only code</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, dolores, cupiditate nisi vel officia cum voluptatem harum voluptatum quia atque esse veritatis quas a magnam repellat, vitae ipsam molestiae est non nesciunt magni quisquam? Quis eveniet dignissimos magni labore ipsa?</p>
+        <button >Explore</button>
+    </section>
+     
+    "?>
+
+</body>
+</html>
